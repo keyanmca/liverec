0b893037950b34d3890981dac9a589e1b39f4c6f
stjosh <josua.hunziker@gmail.com>
2014-03-22 12:32:01 +0100

    Installed Silex and created a "Hello World"

---
+++ web/index.php
@@ -0,0 +1,8 @@
+// Index file for development - use with PHP 5.4 Web Server
+
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
+require __DIR__.'/../src/liverec.php';
